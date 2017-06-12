12b46b79355d5f9874f6b705910698392717ad33
mahesh <mahesh.n@testyantra.com>
2017-06-12 16:26:23 +0530

    new commitment

---
+++ Actitime/src/test/java/practice/Demo1.java
@@ -0,0 +1,14 @@
+package practice;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Demo1 {
+	public static void main(String[] args)
+	{
+		
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://www.google.com");
+	}
+
+}
